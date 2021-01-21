789ac26d9082ed34b5bab0e16eb47de3576f7a4d
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2021-01-21 12:12:48 +0100

    Updated homepage with new blocks

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,11 +29,13 @@ class HomeIndex extends React.Component {
                     <section id="one" className="tiles">
                         {/*<ArticleTile title="Klantcases" content="Mooie projecten verdienen aandacht" link="/klantcases" background={elgentos15}  />*/}
                         <ArticleTile title="Team" content="Techneuten met passie voor code" link="/team" background={this.props.data.splash6}  />
-                        <ArticleTile title="Blog" content="Kennisdeling vinden we leuk!" link="/blog" background={this.props.data.splash4}  />
-                        {/*<ArticleTile title="Magento 2" content="Waarom overstappen loont" link="/magento2" background={elgentos4}  />*/}
+                        <ArticleTile title="Cases" content="Een paar projecten in de schijnwerper" link="/cases" background={this.props.data.splash4}  />
+                        <ArticleTile title="Klanten" content="Onze fijne klanten" link="/klanten" background={this.props.data.splash22}  />
+                        <ArticleTile title="Blog" content="Kennisdeling vinden we leuk!" link="/blog" background={this.props.data.splashWall}  />
+                        {/*<ArticleTile title="Magento 2" content="Waarom overstappen loont" link="/magento2" background={this.props.data.splash22}  />*/}
+                        {/*<ArticleTile title="Vacatures" content="Wij zoeken versterking!" link="/vacatures" background={this.props.data.splash22}  />*/}
                         <ArticleTile title="Tech stack" content="Onze favoriete tech om jouw doelen mee te behalen" link="/techstack" background={this.props.data.splash15}  />
-                        <ArticleTile title="Vacatures" content="Wij zoeken versterking!" link="/vacatures" background={this.props.data.splash22}  />
-                        {/*<ArticleTile title="Contact" content="Koffie?" link="/contact" background={elgentos3}  />*/}
+                        <ArticleTile title="Contact" content="Koffie?" link="/contact" background={this.props.data.splashCoffee}  />
                     </section>
                     <FooterCta title="Magento 2 webshops" content="Wil je een magento webshop beginnen of je bestaande magento webshop optimaliseren? elgentos is gespecialiseerd in het begeleiden van het 'denkproces over de webshops', het ontwerpen-, het ontwikkelen en onderhouden van webshops. Mooie, stabiele en goed converterende webshops bouwen, dat is het uitgangspunt van waaruit wij werken. We zijn in 2010 begonnen met webshops bouwen op Magento en dit is sindsdien onze primaire focus gebleven." link="/magento2" cta="Waarom Magento 2?" />
                 </div>
@@ -91,7 +93,31 @@ export const pageQuery = graphql`
           sizes
         }
     }
+    splashCoffee: imageSharp(fixed:{originalName: {eq:"elgentos-3.jpg"}}) {
+        sizes {
+          base64
+          tracedSVG
+          aspectRatio
+          src
+          srcSet
+          srcWebp
+          srcSetWebp
+          sizes
+        }
+    }
     splash6: imageSharp(fixed:{originalName: {eq:"elgentos-6.jpg"}}) {
+        sizes {
+          base64
+          tracedSVG
+          aspectRatio
+          src
+          srcSet
+          srcWebp
+          srcSetWebp
+          sizes
+        }
+    }
+    splashWall: imageSharp(fixed:{originalName: {eq:"elgentos-17.jpg"}}) {
         sizes {
           base64
           tracedSVG
